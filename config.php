c3683dbf8fef93294d2cb6ed1b64ff2426dcb95a
shashank9119 <118582678+shashank9119@users.noreply.github.com>
2025-01-06 16:40:15 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Database configuration
-$host = "localhost";  // or your database server (e.g., "localhost")
+$host = "mywebappname989.azurewebsites.net";  // or your database server (e.g., "localhost")
 $username = "shashank";   // your MySQL username (default is "root" for XAMPP)
 $password = "Login@123#456";       // your MySQL password (default is empty for XAMPP)
 $database = "mysqldb989"; // your database name
