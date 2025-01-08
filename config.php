f9982062cf1d72780178a203425f35da8bc3fb09
shashank9119 <118582678+shashank9119@users.noreply.github.com>
2025-01-08 17:48:16 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$host = "myserverdb989.database.windows.net";  // Azure SQL Server hostname
+$host = "sqlserver898new.database.windows.net";  // Azure SQL Server hostname
 $username = "shashank";   // Database username
 $password = "Login@123#456";  // Database password
-$database = "mysqldb989"; // Database name
+$database = "appdb"; // Database name
 
 // Create connection using PDO
 try {
